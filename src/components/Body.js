cdc2908c4a224c3cbe9929a991e8994640fdd777
Akash Yadav <akashcse7054@gmail.com>
2023-04-27 13:12:56 +0530

    config driven UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,35 +1,18 @@
-const RestaurantCard = () =>{
-    return(
-        <div className="card">
-            <img 
-            src="https://b.zmtcdn.com/data/pictures/9/18469259/53f9af024eb7b70be4fc340d80fa70ac_o2_featured_v2.jpg?output-format=webp"
-            />
-            <h3>Rajasthani Rasoi</h3>
-            <h4>Rajasthani, North india</h4>
-            <h5>4.2 starts</h5>
+import { restaurantList } from "./constants";
+import RestaurantCard from "../restaurantCard";
 
-        </div>
-    );
-}
+// Body Component for body section: It contain all restaurant cards
+// We are mapping restaurantList array and passing data to RestaurantCard component as props with unique key as index
 
 
-const Body = () =>{
-    return(
-     <div className="RestaurentList">
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-     </div>
-    );
- };
+const Body = () => {
+  return (
+    <div className="restaurant-list">
+      {restaurantList.map((restaurant) => {
+        return <RestaurantCard key={restaurant.data.id} {...restaurant.data} />;
+      })}
+    </div>
+  );
+};
 
  export default Body;
