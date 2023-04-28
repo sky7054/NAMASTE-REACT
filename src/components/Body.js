6cb09be64a93d85b9074e11f5f98b9a6635e1979
Akash Yadav <akashcse7054@gmail.com>
2023-04-28 14:29:10 +0530

    filter element

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,109 @@
-import { restaurantList } from "./constants";
-import RestaurantCard from "../restaurantCard";
+// import { restaurantList } from "./constants";
+// import RestaurantCard from "../restaurantCard";
+// import { useState } from "react";
+
+// function filterData(searchText, restaurants) {
+//   const filterData = restaurants.filter((restaurant) =>
+//    restaurant?.data?.name.toLowerCase().includes(searchText.toLowerCase())
+
+//   );
+//   return filterData;
+// }
+
+
+// const Body = () => {
+
+//   const [restaurants, setRestaurants] = useState(restaurantList);
+//   const[searchText,setSearchText] = useState("");
+//   return (
+    
+//     <>
+//     <div className="search-container">
+//       <input 
+//       type="text" 
+//       className = "search-input" 
+//       placeholder="search" 
+//       value={searchText}
+//       onChange = {(e) =>{
+//         setSearchText(e.target.value);
+        
+//       }}
+//       >
+     
+//     </input>
+
+//     <button
+//     className="search-btn"
+//     onClick={() => {
+//       // filter the data
+//       const data = filterData(searchText, restaurants);
+//       // update the state of restaurants list
+//       setRestaurants(data);
+//     }}>
+//       Search
+//       </button>
+//     </div>
+//     <div className="restaurant-list">
+//       {restaurantList.map((restaurant) => {
+//         return <RestaurantCard key={restaurant.data.id} {...restaurant.data} />;
+//       })}
+//     </div>
+//     </>
+//   );
+// };
+
+ //export default Body;
+
+
+
+ import { restaurantList } from "./constants";
+import RestaurantCard from "../RestaurantCard";
+import { useState } from "react";
+
+function filterData(searchText, restaurants) {
+  const filterData = restaurants.filter((restaurant) =>
+    restaurant?.data?.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+  return filterData;
+}
 
 // Body Component for body section: It contain all restaurant cards
-// We are mapping restaurantList array and passing data to RestaurantCard component as props with unique key as index
-
-
+// We are mapping restaurantList array and passing JSON data to RestaurantCard component as props with unique key as index
 const Body = () => {
+  // useState: To create a state variable, searchText is local state variable
+  const [searchText, setSearchText] = useState("");
+  const [restaurants, setRestaurants] = useState(restaurantList);
   return (
-    <div className="restaurant-list">
-      {restaurantList.map((restaurant) => {
-        return <RestaurantCard key={restaurant.data.id} {...restaurant.data} />;
-      })}
-    </div>
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search a restaurant you want..."
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        ></input>
+        <button
+          className="search-btn"
+          onClick={() => {
+            // filter the data
+            const data = filterData(searchText, restaurants);
+            // update the state of restaurants list
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="restaurant-list">
+        {restaurants.map((restaurant) => {
+          return (
+            <RestaurantCard key={restaurant.data.id} {...restaurant.data} />
+          );
+        })}
+      </div>
+    </>
   );
 };
 
- export default Body;
+export default Body;
