9cabf4f6824ade51f753f745770ed3bae60cbc4b
Akash Yadav <akashcse7054@gmail.com>
2023-05-19 14:10:12 +0530

    chapter-11

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -1,9 +1,33 @@
+const section = (title,description) => {
+    return(
+        <div className="section-border">
+            <h1>{title}</h1>
+            <p>{description}</p>
+        </div>
+
+    );
+};
 
 const InstaMart = () =>{
     return(
         <div>
-            <h1>Instamart</h1>
-            <h2>there are 1000 types of product are available</h2>
+            <h1 className="InstaMart">InstaMart</h1>
+
+            <section 
+            title = {"About Instamart"}
+            description = {"this is about section of InstaMart"}
+            />
+
+            <section 
+            title = {"Team Instamart"}
+            description = {"this is Team section of InstaMart"}
+            />
+
+            <section 
+            title = {"Careers Instamart"}
+            description = {"this is Careers section of InstaMart"}
+            />
+
         </div>
     );
 };
